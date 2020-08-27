d8016ba6b478420c926f5faf93da2309538fac32
stephen <stephenmysql@163.com>
2020-08-27 09:16:27 +0800

    修改代理销售总额为找出下级平级并计算总额

--- test1.py
+++ test1.py
@@ -16,10 +16,10 @@ now_time = datetime.datetime.strftime(now_time_r,'%Y-%m-%d_%H_%M')
 
 class cnMySQL:
     def __init__(self):
-        self._dbhost = '101.37.79.230'
-        self._dbuser = 'root'
-        self._dbpassword = 'mysql_pass'
-        self._dbname = 'xitu'
+        self._dbhost = 'rm-bp1cim09l95sf67dplo.mysql.rds.aliyuncs.com'
+        self._dbuser = 'python_dba'
+        self._dbpassword = 'Python_dba1'
+        self._dbname = 'prod_user'
         self._dbcharset = 'utf8'
         self._dbport = int(3306)
         self._conn = self.connectMySQL()
@@ -52,14 +52,14 @@ class cnMySQL:
             except Exception:
                 print("关闭数据库连接异常")
 
-    def ExecQuery(self,sql):
+    def ExecQuery(self,sql,*args):
         """
         执行查询语句
         """
         res = ''
         if (self._conn):
             try:
-                self._cursor.execute(sql)
+                self._cursor.execute(sql,args)
                 res = self._cursor.fetchall()
             except Exception:
                 res = False
@@ -101,8 +101,238 @@ def kmtest():
     return
 
 
+def total_cash_final():
+    """
+    循环找出所有下级代理
+    """
+    conn = cnMySQL()
+    final_rows = []
+    #upper_id_tuple = (409,419,422,432,434,441,443,447,451,456,467,481,485,487,488,490,492,507,27702,30878,95324)
+    upper_id_tuple = (432,)
+    while True:
+        #check_sql = 'select id from renren_distributor where  ref_id in '' and level_id =3'
+        rows = conn.ExecQuery("select id from renren_distributor where  ref_id in %s and level_id =2",upper_id_tuple)
+        if not rows:
+            break
+        final_rows.extend(rows)
+        upper_id_tuple=tuple(row['id'] for row in rows)
+    print(final_rows)
+
+
+
+
+
+def total_cash_final_cash(proxy_id):
+    """
+    循环找出所有下级代理，并计算他们的销售总额
+    """
+    conn = cnMySQL()
+    final_rows = []
+    #upper_id_tuple = (409,419,422,432,434,441,443,447,451,456,467,481,485,487,488,490,492,507,27702,30878,95324)
+    cansai_id = (proxy_id,)
+    upper_id_tuple = (proxy_id,)
+    while True:
+        rows = conn.ExecQuery("select id from renren_distributor where  ref_id in %s and level_id =2",upper_id_tuple)
+        if not rows:
+            break
+        final_rows.extend(rows)
+        upper_id_tuple=tuple(row['id'] for row in rows)
+    print(final_rows)
+    #获取sql的where条件的 tuple
+    sql_condition_1 = tuple(i['id'] for i in final_rows)
+    sql_condition = cansai_id + sql_condition_1
+    print(sql_condition)
+    total_cash_result = conn.ExecQuery("select  a.NAME 代理姓名,\
+                                        a.auth_code 授权码,\
+                                        case a.level_id\
+                                            when '1' then '分公司'\
+                                            when '2' then '合伙人'\
+                                            when '3' then '官方'\
+                                            when '4' then '省代'\
+                                            when '5' then '市代'\
+                                            when '6' then '会员'\
+                                            else '其他' end as 代理等级,\
+                                        sum(b.payment)  订单总额单位分,\
+                                        case a.region_id\
+                                            when '1' then '笛梦大区'\
+                                            when '2' then '环球大区'\
+                                            when '3' then '辉煌大区'\
+                                            when '4' then '聚米大区'\
+                                            when '5' then '聚星大区'\
+                                            when '6' then '口口大区'\
+                                            when '7' then '米苏大区'\
+                                            when '8' then '野狼大区'\
+                                            when '9' then '海纳百川大区'\
+                                            when '10' then '红红大区'\
+                                            when '11' then '熊熊大区'\
+                                            when '12' then '飞越大区'\
+                                            when '13' then '测试大区'\
+                                            else '其他' end as 进货人所属大区\
+                                    from \
+                                        prod_user.renren_distributor a,\
+                                        prod_order.rupt_purchase_order b\
+                                    where  a.id = b.source_id \
+                                    and b.status in (6,7,8,9) and a.id in  %s",sql_condition)
+    print(total_cash_result)
+    #查询一个代理的销售总额并返回结果
+    return total_cash_result
+
+def totalStatistics():
+    """
+    分别查询每个参赛代理的销售总额，形成最终的报表并写进xlsx文件
+    """
+    canshai_id_tuple = (409,419,422,432,434,441,443,447,451,456,467,481,485,487,488,490,492,507,27702,30878,95324)
+    Statistics_rows = []
+    for i  in  canshai_id_tuple:
+        totoal_cash_result = total_cash_final_cash(i)
+        Statistics_rows.append(total_cash_result)
+    # 将最后结果写进文件
+    headers = ['代理姓名','授权码','代理等级','订单总额单位分','进货人所属大区']
+    dt = pd.DataFrame(sales,columns=headers)
+    dt.to_excel(totalsales_xlsx,index=0)
+    return
+
+
+def total_cash_final_cash_together():
+    """
+    循环找出所有下级代理，并计算他们的销售总额
+    """
+    #canshai_id_tuple = (409,419,422,432,434,441,443,447,451,456,467,481,485,487,488,490,492,507,27702,30878,95324)
+    canshai_id_tuple = (419,)
+    Statistics_rows = []
+    for i  in  canshai_id_tuple:
+        conn = cnMySQL()
+        final_rows = []
+        cansai_id = (i,)
+        upper_id_tuple = (i,)
+        while True:
+            rows = conn.ExecQuery("select id from renren_distributor where  ref_id in %s and level_id =2",upper_id_tuple)
+            if not rows:
+                break
+            final_rows.extend(rows)
+            upper_id_tuple=tuple(row['id'] for row in rows)
+        print(final_rows)
+        #获取sql的where条件的 tuple
+        sql_condition_1 = tuple(i['id'] for i in final_rows)
+        sql_condition = cansai_id + sql_condition_1
+        print(sql_condition)
+        total_cash_result = conn.ExecQuery("select\
+                                        d.id 代理id,\
+                                        d.NAME 代理姓名,\
+                                        d.auth_code 授权码,\
+                                        case d.level_id\
+                                            when '1' then '分公司'\
+                                            when '2' then '合伙人'\
+                                            when '3' then '官方'\
+                                            when '4' then '省代'\
+                                            when '5' then '市代'\
+                                            when '6' then '会员'\
+                                            else '其他' end as 代理等级,\
+                                        case d.region_id\
+                                            when '1' then '笛梦大区'\
+                                            when '2' then '环球大区'\
+                                            when '3' then '辉煌大区'\
+                                            when '4' then '聚米大区'\
+                                            when '5' then '聚星大区'\
+                                            when '6' then '口口大区'\
+                                            when '7' then '米苏大区'\
+                                            when '8' then '野狼大区'\
+                                            when '9' then '海纳百川大区'\
+                                            when '10' then '红红大区'\
+                                            when '11' then '熊熊大区'\
+                                            when '12' then '飞越大区'\
+                                            when '13' then '测试大区'\
+                                            else '其他' end as 进货人所属大区,\
+                                            aa.total_cash\
+                                    from  prod_user.renren_distributor d,(select  \
+                                        sum(b.payment)  total_cash\
+                                    from \
+                                        prod_user.renren_distributor a,\
+                                        prod_order.rupt_purchase_order b\
+                                    where  a.id = b.source_id \
+                                    and b.status in (6,7,8,9) and a.id in  %s) aa where d.id in %s",sql_condition,cansai_id)
+        print(total_cash_result)
+        Statistics_rows.extend(total_cash_result)
+    print(Statistics_rows)
+    return 
+
+def total_cash_final_cash(proxy_id):
+    """
+    循环找出所有下级代理，并计算他们的销售总额
+    """
+    conn = cnMySQL()
+    final_rows = []
+    cansai_id = (proxy_id,)
+    upper_id_tuple = (proxy_id,)
+    while True:
+        rows = conn.ExecQuery("select id from renren_distributor where  ref_id in %s and level_id =2",upper_id_tuple)
+        if not rows:
+            break
+        final_rows.extend(rows)
+        upper_id_tuple=tuple(row['id'] for row in rows)
+    #print(final_rows)
+    #获取sql的where条件的 tuple
+    sql_condition_1 = tuple(i['id'] for i in final_rows)
+    sql_condition = cansai_id + sql_condition_1
+    #print(sql_condition)
+    total_cash_result = conn.ExecQuery("select\
+                                        d.id 代理id,\
+                                        d.NAME 代理姓名,\
+                                        d.auth_code 授权码,\
+                                        case d.level_id\
+                                            when '1' then '分公司'\
+                                            when '2' then '合伙人'\
+                                            when '3' then '官方'\
+                                            when '4' then '省代'\
+                                            when '5' then '市代'\
+                                            when '6' then '会员'\
+                                            else '其他' end as 代理等级,\
+                                        case d.region_id\
+                                            when '1' then '笛梦大区'\
+                                            when '2' then '环球大区'\
+                                            when '3' then '辉煌大区'\
+                                            when '4' then '聚米大区'\
+                                            when '5' then '聚星大区'\
+                                            when '6' then '口口大区'\
+                                            when '7' then '米苏大区'\
+                                            when '8' then '野狼大区'\
+                                            when '9' then '海纳百川大区'\
+                                            when '10' then '红红大区'\
+                                            when '11' then '熊熊大区'\
+                                            when '12' then '飞越大区'\
+                                            when '13' then '测试大区'\
+                                            else '其他' end as 进货人所属大区,\
+                                            aa.total_cash\
+                                    from  prod_user.renren_distributor d,(select  \
+                                        sum(b.payment)  total_cash\
+                                    from \
+                                        prod_user.renren_distributor a,\
+                                        prod_order.rupt_purchase_order b\
+                                    where  a.id = b.source_id \
+                                    and b.status in (6,7,8,9) and a.id in  %s) aa where d.id in %s",(sql_condition,))
+    #print(total_cash_result)
+    #查询一个代理的销售总额并返回结果
+    return total_cash_result
+
+def totalStatistics():
+    """
+    分别查询每个参赛代理的销售总额，形成最终的报表并写进xlsx文件
+    """
+    canshai_id_tuple = (409,419,422,432,434,441,443,447,451,456,467,481,485,487,488,490,492,507,27702,30878,95324)
+    Statistics_rows = []
+    for i  in  canshai_id_tuple:
+        totoal_cash_result_xlsx = total_cash_final_cash(i)
+        Statistics_rows.append(total_cash_result_xlsx)
+    print(Statistics_rows)
+    # 将最后结果写进文件
+    headers = ['代理姓名','授权码','代理等级','订单总额单位分','进货人所属大区']
+    dt = pd.DataFrame(Statistics_rows,columns=headers)
+    dt.to_excel(totalsales_xlsx,index=0)
+    return
+
+
 def main():
-    kmtest()
+    total_cash_final_cash_together()
     return
 
 if __name__ == '__main__':
--- total_sales.py
+++ total_sales.py
@@ -3,7 +3,7 @@
 # @Author  : Stephev
 # @Site    : 
 # @File    : total_sales.py
-# @Remarks :每周五10点提取代理的销售进货总额发送到邮箱
+# @Remarks :每周五10点提取代理的销售进货总额发送到邮箱,写程序循环去查
 
 
 import pymysql
@@ -16,7 +16,6 @@ from email.header import Header
 import pandas as pd
 
 
-
 now_time_r = datetime.datetime.now()
 now_time = datetime.datetime.strftime(now_time_r,'%Y-%m-%d_%H_%M')
 totalsales_xlsx = "E:\\csv_file\\"+now_time+"_代理进货总额.xlsx"
@@ -27,7 +26,7 @@ class cnMySQL:
         self._dbhost = 'rm-bp1cim09l95sf67dplo.mysql.rds.aliyuncs.com'
         self._dbuser = 'python_dba'
         self._dbpassword = 'Python_dba1'
-        self._dbname = 'prod_order'
+        self._dbname = 'prod_user'
         self._dbcharset = 'utf8'
         self._dbport = int(3306)
         self._conn = self.connectMySQL()
@@ -60,14 +59,14 @@ class cnMySQL:
             except Exception:
                 print("关闭数据库连接异常")
 
-    def ExecQuery(self,sql):
+    def ExecQuery(self,sql,*args):
         """
         执行查询语句
         """
         res = ''
         if (self._conn):
             try:
-                self._cursor.execute(sql)
+                self._cursor.execute(sql,args)
                 res = self._cursor.fetchall()
             except Exception:
                 res = False
@@ -75,52 +74,71 @@ class cnMySQL:
             self.close()
         return res
 
-
-
-def totalSales():
+def total_cash_final_cash_together():
     """
-    提取所有代理的账户余额
+    循环找出所有下级代理，并计算他们的销售总额
     """
-    conn = cnMySQL()
-    totalsales_sql = "select a.NAME 代理姓名,\
-                        a.auth_code 授权码,\
-                        case a.level_id\
-                            when '1' then '分公司'\
-                            when '2' then '合伙人'\
-                            when '3' then '官方'\
-                            when '4' then '省代'\
-                            when '5' then '市代'\
-                            when '6' then '会员'\
-                            else '其他' end as 代理等级,\
-                        sum(b.payment)  订单总额单位分,\
-                        case a.region_id\
-                            when '1' then '笛梦大区'\
-                            when '2' then '环球大区'\
-                            when '3' then '辉煌大区'\
-                            when '4' then '聚米大区'\
-                            when '5' then '聚星大区'\
-                            when '6' then '口口大区'\
-                            when '7' then '米苏大区'\
-                            when '8' then '野狼大区'\
-                            when '9' then '海纳百川大区'\
-                            when '10' then '红红大区'\
-                            when '11' then '熊熊大区'\
-                            when '12' then '飞越大区'\
-                            when '13' then '测试大区'\
-                            else '其他' end as 进货人所属大区\
-                    from \
-                        prod_user.renren_distributor a,\
-                        prod_order.rupt_purchase_order b,\
-                        prod_user.user d\
-                    where  a.id = b.source_id \
-                    and  a.account_id = d.id \
-                    and b.status in (6,7,8,9)  group by a.id;"
-    sales = conn.ExecQuery(totalsales_sql)
-    headers = ['代理姓名','授权码','代理等级','订单总额单位分','进货人所属大区']
-    dt = pd.DataFrame(sales,columns=headers)
+    canshai_id_tuple = (409,419,422,432,434,441,443,447,451,456,467,481,485,487,488,490,492,507,27702,30878,95324)
+    Statistics_rows = []
+    for i  in  canshai_id_tuple:
+        conn = cnMySQL()
+        final_rows = []
+        cansai_id = (i,)
+        upper_id_tuple = (i,)
+        while True:
+            rows = conn.ExecQuery("select id from renren_distributor where  ref_id in %s and level_id =2",upper_id_tuple)
+            if not rows:
+                break
+            final_rows.extend(rows)
+            upper_id_tuple=tuple(row['id'] for row in rows)
+        #print(final_rows)
+        #获取sql的where条件的 tuple
+        sql_condition_1 = tuple(i['id'] for i in final_rows)
+        sql_condition = cansai_id + sql_condition_1
+        #print(sql_condition)
+        total_cash_result = conn.ExecQuery("select\
+                                        d.id 代理id,\
+                                        d.NAME 代理姓名,\
+                                        d.auth_code 授权码,\
+                                        case d.level_id\
+                                            when '1' then '分公司'\
+                                            when '2' then '合伙人'\
+                                            when '3' then '官方'\
+                                            when '4' then '省代'\
+                                            when '5' then '市代'\
+                                            when '6' then '会员'\
+                                            else '其他' end as 代理等级,\
+                                        case d.region_id\
+                                            when '1' then '笛梦大区'\
+                                            when '2' then '环球大区'\
+                                            when '3' then '辉煌大区'\
+                                            when '4' then '聚米大区'\
+                                            when '5' then '聚星大区'\
+                                            when '6' then '口口大区'\
+                                            when '7' then '米苏大区'\
+                                            when '8' then '野狼大区'\
+                                            when '9' then '海纳百川大区'\
+                                            when '10' then '红红大区'\
+                                            when '11' then '熊熊大区'\
+                                            when '12' then '飞越大区'\
+                                            when '13' then '测试大区'\
+                                            else '其他' end as 进货人所属大区,\
+                                            aa.total_cash 订单总额单位分\
+                                    from  prod_user.renren_distributor d,(select  \
+                                        sum(b.total_fee)  total_cash\
+                                    from \
+                                        prod_user.renren_distributor a,\
+                                        prod_order.rupt_purchase_order b\
+                                    where  a.id = b.source_id \
+                                    and b.status in (6,7,8,9) and a.id in  %s) aa where d.id in %s",sql_condition,cansai_id)
+        #print(total_cash_result)
+        Statistics_rows.extend(total_cash_result)
+    print(Statistics_rows)
+    # 将最后结果写进文件
+    headers = ['代理id','代理姓名','授权码','代理等级','进货人所属大区','订单总额单位分']
+    dt = pd.DataFrame(Statistics_rows,columns=headers)
     dt.to_excel(totalsales_xlsx,index=0)
     return
-
 
 def sendMail():
     mail_host='smtp.qq.com'
@@ -128,7 +146,7 @@ def sendMail():
     mail_pass='wcasswmrgsnobdgh'
 
     sender='1058582934@qq.com'
-    #receivers = ['pd@xitu.com','chenxing@xitu.com','cj@xitu.com','sxl@xitu.com','dxy@xitu.com','xjj@xitu.com','gaowei@xitu.com']
+    #receivers = ['gaowei@xitu.com']
     receivers=['gaowei@xitu.com','xulanfen@xitu.com','huyaqinhyq@xitu.com','yuzhangying@xitu.com','lb@xitu.com']
     
     #创建一个带附件的实例
@@ -139,7 +157,7 @@ def sendMail():
     message['Subject'] = Header(subject, 'utf-8')
 
     #邮件正文内容
-    message.attach(MIMEText('hi:\n    附件是每周五代理进货总额明细，请查收。 如有问题可与我联系\n\n\n技术中心-高巍', 'plain', 'utf-8'))
+    message.attach(MIMEText('hi:\n    附件是每周五代理进货总额明细，请查收。此次为测试，如有问题可与我联系\n\n\n   说明：1.进货总额为个人及其下级等级为合伙人代理的 所有进货订单总额之和，不含运费 \n      2.计入统计的订单状态为代发货，调货中，待收货，完成。\n\n\n技术中心-高巍', 'plain', 'utf-8'))
 
     # 构造附件1，传送当前目录下的 totalsales_xlsx 文件
     att1 = MIMEText(open(totalsales_xlsx, 'rb').read(), 'base64', 'utf-8')
@@ -158,9 +176,8 @@ def sendMail():
         print("Error: 无法发送邮件")
     return
 
-
 def main():
-    totalSales()
+    total_cash_final_cash_together()
     sendMail()
     return
 
